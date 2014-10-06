d70daa7bdc902d982e343a49ff69234a42cb1251
Cassy Jens <CassyJens@users.noreply.github.com>
2014-10-05 23:47:59 -0700

    Delete example1.js

--- example1.js
+++
@@ -1,8 +0,0 @@
-// my node program
-// sends a random tweet on input
-var pfio = require('piface-node');
-pfio.init();
-pfio.digital_write(0,1); // (pin, state)
-var foo = pfio.digital_read(0); // (pin; returns state)
-pfio.deinit();
-
